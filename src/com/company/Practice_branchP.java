9d2199c1d83d4f1799cf35166e26bed327e4b953
SabryneRjeb <sabryne.rjeb@gmail.com>
2021-01-02 21:50:48 -0500

    Practicing new branch part 2

--- src/com/company/Practice_branchP.java
+++ src/com/company/Practice_branchP.java
@@ -3,5 +3,7 @@ package com.company;
 public class Practice_branchP {
     public static void main(String[] args) {
         System.out.println("Practice more branching");
+        // more info for practice
+        // more work to show off to Muhtar
     }
 }
